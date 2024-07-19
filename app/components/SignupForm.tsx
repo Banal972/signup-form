ed2a6f4b491f099e907d1dd5367bb233f4a83535
Kim Ji Yu <spbabo97@naver.com>
2024-07-19 14:20:31 +0900

    과제

--- app/components/SignupForm.tsx
+++ app/components/SignupForm.tsx
@@ -41,20 +41,26 @@ const SignupForm: React.FC = () => {
     if (password === "") return "값을 입력해주세요.";
     if (password.length < 8) return "최소 8자 이상 입력해주세요.";
     if (password.length > 20) return "최대 20자 이하로 입력해주세요.";
+    if (!/^[a-zA-Z0-9]*$/.test(password)) return "영문과 숫자만 입력해주세요.";
     if (password !== confirmPassword) return "비밀번호가 일치하지 않습니다.";
     return "";
   };
   const confirmPasswordCheck = (confirmPassword: string) => {
     if (confirmPassword === "") return "값을 입력해주세요.";
     if (confirmPassword.length < 8) return "최소 8자 이상 입력해주세요.";
+    if (!/^[a-zA-Z0-9]*$/.test(confirmPassword))
+      return "영문과 숫자만 입력해주세요.";
     if (confirmPassword.length > 20) return "최대 20자 이하로 입력해주세요.";
+    return "";
+  };
+  const emailCheck = (email: string) => {
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email))
+      return "이메일 형식에 맞게 입력해주세요.";
     return "";
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    let submit = true;
 
     setError({
       id: "",
@@ -70,62 +76,41 @@ const SignupForm: React.FC = () => {
     const nameError = nameCheck(name);
     const passwordError = passwordCheck(password, confirmPassword);
     const confirmPasswordError = confirmPasswordCheck(confirmPassword);
+    const emailError = emailCheck(email);
 
     if (idError || nameError) {
       setError((prev) => ({
         ...prev,
         id: idError,
         name: nameError,
+        email: emailError,
         password: passwordError,
         confirmPassword: confirmPasswordError,
       }));
-      submit = false;
+      return;
     }
 
-    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email)) {
-      setError((prev) => ({
-        ...prev,
-        email: "이메일 형식에 맞게 입력해주세요.",
-      }));
-      submit = false;
-    }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (
-      !/^[a-zA-Z0-9]*$/.test(password) ||
-      !/^[a-zA-Z0-9]*$/.test(confirmPassword)
-    ) {
-      setError((prev) => ({
-        ...prev,
-        email: "영문과 숫자만 입력해주세요.",
-      }));
-      submit = false;
-    }
+    const user = {
+      id,
+      name,
+      email,
+      password,
+      hashedPassword,
+    };
 
-    if (submit) {
-      const hashedPassword = await bcrypt.hash(password, 10);
+    if (localStorage.getItem("users")) {
+      const datas = JSON.parse(localStorage.getItem("users") || "[]") as any[];
+      const find = datas.findIndex((data) => data.email === email);
 
-      const user = {
-        id,
-        name,
-        email,
-        password,
-        hashedPassword,
-      };
-
-      if (localStorage.getItem("users")) {
-        const datas = JSON.parse(
-          localStorage.getItem("users") || "[]"
-        ) as any[];
-        const find = datas.findIndex((data) => data.email === email);
-
-        if (find > -1) {
-          return setSubmitError("이미 존재하는 이메일입니다.");
-        } else {
-          localStorage.setItem("users", JSON.stringify([...datas, user]));
-        }
+      if (find > -1) {
+        return setSubmitError("이미 존재하는 이메일입니다.");
       } else {
-        localStorage.setItem("users", JSON.stringify([user]));
+        localStorage.setItem("users", JSON.stringify([...datas, user]));
       }
+    } else {
+      localStorage.setItem("users", JSON.stringify([user]));
     }
   };
 
